887b6ce2be6d31c8dd41f2314e5518f2bb7963b4
Danny Aballay <daballayf@gmail.com>
2022-07-28 09:55:11 -0400

    Restaurant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating restaurants"
+Restaurant.destroy_all
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: '11111', category: 'french'}
+la_hacienda = { name: "La Hacienda", address: "Isidora Goyenechea", phone_number: '22222', category: 'italian'}
+wenwa = { name: "Wenwa", address: "Vitacura 12334", phone_number: '33333', category: 'japanese'}
+tiramisu = { name: "Tiramisu", address: "Isidora Goyenechea", phone_number: '44444', category: 'italian'}
+la_perla = { name: "la Perla", address: "Bilbao 1234", phone_number: '55555', category: 'chinese'}
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St", phone_number: '77777', category: 'belgian'}
+
+restaurants = [dishoom, la_hacienda, wenwa, tiramisu, la_perla, pizza_east]
+restaurants.each do |key|
+  restaurant = Restaurant.new(key)
+  restaurant.save
+  puts "creado: #{restaurant.name}"
+end
